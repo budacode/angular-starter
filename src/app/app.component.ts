8529c0268a097ec59c90a680c89d126479352f54
Marcell Huszti <husztim.ext@astron.hu>
2018-03-08 11:23:48 +0100

    chore: add husky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: require('pug-loader!./app.component.pug'),
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 }
