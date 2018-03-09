67abd4e8a353adf5eca9259ab8890bd683bb1ccc
Marcell Huszti <husztim.ext@astron.hu>
2018-03-09 12:41:57 +0100

    fix: minor issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: require('pug-loader!./app.component.pug'),
+  template: require('pug-loader!./app.component.pug')(),
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
